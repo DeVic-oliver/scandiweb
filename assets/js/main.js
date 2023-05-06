ebd8b1de008031da5d292aa6edf8348a3cab1f98
Victor Matheus <victormatheus183@gmail.com>
2023-05-06 12:53:22 -0300

    javascript toggle product type switcher

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,15 @@
 $(()=>{
+    const cardTypes =  Array.from($('.product-type-card'));
+    $('#productType').on('change', ()=>{
+        var selectedProductType = $('#productType').find(":selected").text().toLowerCase();
+        cardTypes.forEach(element => {
+            let cardTypeID = element.id.toLowerCase(); 
+            if(cardTypeID === selectedProductType){
+                $('#'+element.id).css("display", "block");
+            }else{
+                $('#'+element.id).css("display", "none");
+            }
+        });
+    })
+
 })
